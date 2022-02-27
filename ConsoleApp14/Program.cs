3addd7ec7e884f59a4b03a2c03fa61a4dc57f0f6
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:13:52 +0800

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for(int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+Console.ReadLine();
